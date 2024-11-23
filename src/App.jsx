df41ddbc7b2aa151e16a7a094a1229dc0eb358c6
himanshuguptait09 <himanshuguptait09@gmail.com>
2024-11-23 10:18:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import OtherDeatils from "./componets/OtherDeatils"
-import "./App.css"
+import React from "react";
+import OtherDeatils from "./componets/OtherDeatils";
+import "./App.css";
+import Visit from "./componets/visit";
 const App = () => {
   return (
-    <div ><OtherDeatils/></div>
-  )
-}
+    <div>
+      <OtherDeatils />
+      <Visit />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/componets/Visit.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+
+const VisitDetails = () => {
+  const [formValues, setFormValues] = useState({
+    billCategory: "",
+    corporate: "",
+    enrollmentId: "",
+    enrollmentName: "",
+    approvalNo: "",
+    refLetterNo: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Handle reset of "Corporate" when "Bill Category" changes
+    if (name === "billCategory") {
+      setFormValues({
+        ...formValues,
+        [name]: value,
+        corporate: "", // Reset the "Corporate" field to default
+      });
+    } else {
+      setFormValues({
+        ...formValues,
+        [name]: value,
+      });
+    }
+  };
+
+  return (
+    <div className=" bg-light rounded m-5 py-* pt-3 pb-4">
+      <div className="container mt-4">
+        <h5 className="mb-3">Visit Details</h5>
+        <div className="row g-3">
+          {/* Bill Category */}     
+          <div className="col-md-2">
+            <label htmlFor="billCategory" className="form-label">
+              Bill Category <span className="text-danger">*</span>
+            </label>
+            <select
+              id="billCategory"
+              name="billCategory"
+              className="form-select"
+              value={formValues.billCategory}
+              onChange={handleInputChange}
+            >
+              <option value="">Select</option>
+              <option value="Cash">Cash</option>
+              <option value="Insurance">Insurance</option>
+            </select>
+          </div>
+
+          {/* Corporate */}
+          <div className="col-md-2">
+            <label htmlFor="corporate" className="form-label">
+              Corporate <span className="text-danger">*</span>
+            </label>
+            <select
+              id="corporate"
+              name="corporate"
+              className="form-select"
+              value={formValues.corporate}
+              onChange={handleInputChange}
+            >
+              <option value="">Select</option>
+              <option value="RGC PRIVATE">RGC PRIVATE</option>
+              <option value="Other Corp">Other Corp</option>
+            </select>
+          </div>
+
+          {/* Enrollment ID */}
+          <div className="col-md-2">
+            <label htmlFor="enrollmentId" className="form-label">
+              Enrollment Id
+            </label>
+            <input
+              type="text"
+              id="enrollmentId"
+              name="enrollmentId"
+              className="form-control"
+              value={formValues.enrollmentId}
+              onChange={handleInputChange}
+              placeholder="Enrollment Id"
+            />
+          </div>
+
+          {/* Enrollment Name */}
+          <div className="col-md-2">
+            <label htmlFor="enrollmentName" className="form-label">
+              Enrollment Name
+            </label>
+            <input
+              type="text"
+              id="enrollmentName"
+              name="enrollmentName"
+              className="form-control"
+              value={formValues.enrollmentName}
+              onChange={handleInputChange}
+              placeholder="Enrollment Name"
+            />
+          </div>
+
+          {/* Approval No */}
+          <div className="col-md-2">
+            <label htmlFor="approvalNo" className="form-label">
+              Approval No.
+            </label>
+            <input
+              type="text"
+              id="approvalNo"
+              name="approvalNo"
+              className="form-control"
+              value={formValues.approvalNo}
+              onChange={handleInputChange}
+              placeholder="Approval No."
+            />
+          </div>
+
+          {/* Ref Letter No */}
+          <div className="col-md-2">
+            <label htmlFor="refLetterNo" className="form-label">
+              Ref. Letter No.
+            </label>
+            <input
+              type="text"
+              id="refLetterNo"
+              name="refLetterNo"
+              className="form-control"
+              value={formValues.refLetterNo}
+              onChange={handleInputChange}
+              placeholder="Ref. Letter No."
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VisitDetails;
